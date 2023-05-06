ed80b8993be7b2df91c5b295f5e1417e52d3963e
IT21291296 <99247893+IT21291296@users.noreply.github.com>
2023-05-06 23:09:36 +0530

    CRUD

---
+++ app/src/main/java/com/example/project1/activities/Jobs.kt
@@ -0,0 +1,52 @@
+package com.example.project1.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.project1.Models.Jobcl
+import com.example.project1.databinding.ActivityJobsBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+
+class Jobs : AppCompatActivity() {
+
+    private lateinit var binding: ActivityJobsBinding
+    private lateinit var database:DatabaseReference
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityJobsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.dPost.setOnClickListener {
+            val title=binding.djTitle.text.toString()
+            val location=binding.dLocation.text.toString()
+            val salary=binding.dSalary.text.toString()
+
+            database=FirebaseDatabase.getInstance().getReference("Jobs")
+            val jobcl= Jobcl(title,location,salary)
+            database.child(title).setValue(jobcl).addOnSuccessListener {
+                binding.djTitle.text.clear()
+                binding.dLocation.text.clear()
+                binding.dSalary.text.clear()
+
+                Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
+
+
+            }
+        }
+
+
+
+        }
+    }
+
+
+
+
---
+++ app/src/main/java/com/example/project1/models/Jobcl.kt
@@ -0,0 +1,5 @@
+package com.example.project1.models
+
+data class Jobcl(val title:String?=null,val location:String?=null,val salary:String?=null){
+
+}
